b5034cf1e84ad1fbf077c65a5c88c2877b036d63
yurbeg <yur.beglaryan@gmail.com>
2024-11-27 14:21:52 +0400

    config react typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+import CryptoList from './pages/cryptoList';
+function App() {
+  return (
+    <div className="App">
+      <CryptoList/>
+    </div>
+  );
+}
+
+export default App;
